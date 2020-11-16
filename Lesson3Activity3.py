c7de137651d73b79deb9cd2b3811efc4be6511b2
ZioKli <zionklinger1@gmail.com>
2020-11-16 16:06:09 -0500

    Outlined lesson

---
+++ Lesson3Activity3.py
@@ -0,0 +1,22 @@
+
+
+from lib.Controller import UDP_Controller
+
+
+# All Code Should be written inside these lines
+#######################################################################################
+
+# Create the new UDP_Controller instance
+
+
+
+# add all 3 variables to your controller variable
+
+
+# please note: the variables on the controller CANNOT be accessed the way most python variables can
+# you must use the Controller.setValue() if you wish to set a variable on the controller,
+# and you must use Controller.getValue() if you wish to read the variable on the controller.
+
+
+
+#######################################################################################
