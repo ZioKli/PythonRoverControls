bcb82bb64c56476bc430396d3098fc4cc39f9d82
ZioKli <zionklinger1@gmail.com>
2020-11-18 11:03:54 -0500

    added more test cases

--- lesson2Activity2pt2.py
+++ lesson2Activity2pt2.py
@@ -18,15 +18,20 @@ def add(x, y):
 # finish writing the subtract function so that it returns the difference between two parameters
 # x and y
 def subtract(x,y):
-
     return
 
 #######################################################################################
 
+
+
+
+# this section of code is used to automatically test the function you wrote
 class test(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5,3), 2)
         self.assertEqual(subtract(213,103), 110)
+        self.assertEqual(subtract(0,0), 0)
+        self.assertEqual(subtract(1,1), 0)
 
 
 
